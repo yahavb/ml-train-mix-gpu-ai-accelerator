af56eb731ed4c283931a42c4b63fa2679a736859
EC2 Default User <ec2-user@ip-172-31-1-180.us-west-2.compute.internal>
2023-09-29 21:25:39 +0000

    limit-steps

--- app/train_kinetics_trn.sh
+++ app/train_kinetics_trn.sh
@@ -1,6 +1,6 @@
 echo "in train_kinetics_trn.sh"
 
-#export MALLOC_ARENA_MAX=32
+export MALLOC_ARENA_MAX=32
 XLA_USE_BF16=1 NEURON_CC_FLAGS='--retry_failed_compilation --internal-max-instruction-limit=10000000 --enable-experimental-O1 --internal-build-with-users --enable-saturate-infinity' torchrun --nproc_per_node=32 train_trn.py \
   --config_file_path config/main.yaml \
   --dataset kinetics-small \
@@ -8,5 +8,5 @@ XLA_USE_BF16=1 NEURON_CC_FLAGS='--retry_failed_compilation --internal-max-instru
   --log_steps 4 \
   --num_epochs 1 \
   --batch_size 1 \
-  --max_steps 1000 \
+  --max_steps 100 \
   --lr 1e-5
