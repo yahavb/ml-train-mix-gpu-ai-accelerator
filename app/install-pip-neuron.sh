e9c582d1d8aa3162846676205af9107924fb21b4
EC2 Default User <ec2-user@ip-172-31-1-180.us-west-2.compute.internal>
2023-09-30 18:43:00 +0000

    install-pip-neuron.sh

--- app/install-pip-neuron.sh
+++ app/install-pip-neuron.sh
@@ -2,7 +2,7 @@
 #
 PIP="python3.8 -m pip"
 
-if poetry search torch-neuronx &>/dev/null; then 
+if poetry search torch-neuronx | grep neuronx &>/dev/null; then 
   ${PIP} config set global.extra-index-url $PIP_REPO 
   ${PIP} install --force-reinstall torch-neuronx==1.13.0.* neuronx-cc==2.* --extra-index-url $PIP_REPO
 fi 
